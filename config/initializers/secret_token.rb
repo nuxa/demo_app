02dd39c3c2ca1d35e692f9438013b7412ac5c447
nuxa <holy.edge.nuxaxtuxtu@gmail.com>
2015-04-08 09:31:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd4c8e2c99db76503bad15c407026f98d45787b8ae520952245da41a81932cc56c8b55068f3d03e62f880e1ab8408bb9b150fb258b2239e9d28a1bdb037fa9015'
